5706d26b0bb24f23bd896bc44e7d58f6968f799d
zhangsen <892944774@qq.com>
2020-03-30 19:39:52 +0800

    调试代码应用

---
+++ test_3_30/test_3_30/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}
+//	return 0;
+//}
+
+//实现代码求1！+2！+3！……+n!
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;//保存最终结果
+//	int n = 0;
+//	int ret = 1;//保存n的阶乘
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int ret = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			ret = ret*j;
+//		}
+//		sum = sum + ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//			arr[i] = 0;
+//	}
+//	return 0;
+//}
+
+//20元喝汽水问题
+//int Emptybottle(int n)
+//{
+//	int ret = 0;
+//	while (n > 1)
+//	{
+//		ret += n / 2;
+//		n = n / 2 + n % 2;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Emptybottle(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
