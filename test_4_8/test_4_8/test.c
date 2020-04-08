8344b1e74859e6608ff181218e6a3f26dcdf0021
zhangsen <892944774@qq.com>
2020-04-08 18:32:26 +0800

    代码

---
+++ test_4_8/test_4_8/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <math.h>
+
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i < 200; i++)
+//	{
+//		int j = 0;
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i%j == 0)
+//				break;
+//		}
+//		if (j > sqrt(i))
+//		{
+//			count++;
+//			printf("%d ", i);
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+//int bit(int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if ((n >> i & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int ret = bit(a);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
